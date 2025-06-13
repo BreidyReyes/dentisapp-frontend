599d4cd17217363c0db844f2104639704acd7beb
BreidyReyes <breidyreyesb1@gmail.com>
2025-06-12 20:47:56 -0500

    app.routes.ts : define rutas, pages/: define componentes (public, privado), disponibilidad.component : define formulario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/public/home/home';
 import { Login } from './pages/public/login/login';
 import { Register } from './pages/public/register/register';
-import { Odontologodisponible } from './pages/private/odontologodisponible/odontologodisponible';
+import { Disponibilidad } from './pages/private/disponibilidad/disponibilidad';
 
 export const routes: Routes = [
     { path: "home" ,component: Home},
     { path: "login" , component: Login},
     { path: "register" , component: Register},
-
-    { path: "admin/disponibilidad", component: Odontologodisponible },
+    { path: "admin/disponibilidad", component: Disponibilidad},
 
     { path: "**", redirectTo:"home", pathMatch:"full"},
     { path: "", redirectTo: "home", pathMatch: "full"},
